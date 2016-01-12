f9d3e717cf4118894a108f452e46c7f0c22c19ac
Kazuaki Oshiba <kazuaki@Kazuaki-no-MacBook-Pro.local>
2016-01-12 19:31:45 +0900

    initial commit

---
+++ src/Assets/GameOverController.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+
+import UnityEngine.UI;
+
+public var sceneName : String;
+
+private var player : PlayerControll;
+private var text : Text;
+private var released : boolean = false;
+
+function Start () {
+	player = GameObject.FindGameObjectWithTag("Player").GetComponent.<PlayerControll>();
+	text = GetComponent.<Text>();
+	text.enabled = false;
+}
+
+function Update () {
+	if (player.gameover) {
+		text.enabled = true;
+		var restart = false;
+		if (Input.GetMouseButtonDown(0)) {
+			restart = true;
+		}
+		if (Input.GetKey(KeyCode.Space)) {
+			if (released) {
+				restart = true;
+			}
+		} else {
+			released = true;
+		}
+		if (restart) {
+			UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+		}
+	} else {
+		text.enabled = false;
+	}
+}
---
+++ src/Assets/ScoreController.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+import UnityEngine.UI;
+
+private var player : PlayerControll;
+private var text : Text;
+
+function Start () {
+	player = GameObject.FindGameObjectWithTag("Player").GetComponent.<PlayerControll>();
+	text = GetComponent.<Text>();
+}
+
+function Update () {
+	text.text = player.score.ToString();
+}
