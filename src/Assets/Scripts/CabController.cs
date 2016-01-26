e03ab3d75460752b4c8adea01814908474c6c4d7
Kazuaki Oshiba <kazusanto@gmail.com>
2016-01-27 00:37:54 +0900

    available to carry some baggage on the cabs

--- src/Assets/Scripts/CabController.cs
+++ src/Assets/Scripts/CabController.cs
@@ -3,24 +3,44 @@ using System.Collections;
 
 public class CabController : MonoBehaviour
 {
-    public bool isPlayer = false;
-    public float MaxSpeed = 5.0f;
-    public float MaxSteering = 20.0f;
-    public float Brake = 0.1f;
+    [SerializeField] private bool m_isPlayer = false;
+    [SerializeField] private float m_MaxSpeed = 5.0f;
+    [SerializeField] private float m_MaxSteering = 1.5f;
+    [SerializeField] private float m_Brake = 0.1f;
+    [SerializeField] private GameObject[] m_Baggages = null;
+    [SerializeField] private string m_BaggageOriginName = "CabBody";
+    [SerializeField] private Vector3 m_BaggageOrigin = Vector3.zero;
 
-
-    private Animator animator = null;
-    private float steering = 0.0f;
-    private float speed = 0.0f;
+    private Animator m_animator = null;
+    private float m_steering = 0.0f;
+    private float m_speed = 0.0f;
 
     // Use this for initialization
     void Start () {
-        animator = GetComponent<Animator>();
+        m_animator = GetComponent<Animator>();
+        if (m_Baggages.Length > 0) {
+            var i = Random.Range(0, m_Baggages.Length + 1);
+            if (i < m_Baggages.Length) {
+                var baggage = GameObject.Instantiate(m_Baggages[i]).transform;
+                var seat = transform.Find(m_BaggageOriginName);
+                var joint = new GameObject("Joint").transform;
+                joint.localPosition = new Vector3(
+                    m_BaggageOrigin.x / seat.localScale.x,
+                    m_BaggageOrigin.z / seat.localScale.z,
+                    m_BaggageOrigin.y / seat.localScale.y);
+                joint.localScale = new Vector3(
+                    1.0f / seat.localScale.x,
+                    1.0f / seat.localScale.z,
+                    1.0f / seat.localScale.y);
+                joint.SetParent(seat, false);
+                baggage.SetParent(joint, false);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update () {
-        if (!isPlayer) {
+        if (!m_isPlayer) {
             return;
         }
         var steered = false;
@@ -29,15 +49,15 @@ public class CabController : MonoBehaviour
         if (Input.GetKey(KeyCode.UpArrow)) {
             accel = 1.0f;
             if (Input.GetKey(KeyCode.RightArrow)) {
-                steering += 0.1f;
-                if (steering > 1.0f) {
-                    steering = 1.0f;
+                m_steering += 0.1f;
+                if (m_steering > 1.0f) {
+                    m_steering = 1.0f;
                 }
                 steered = true;
             } else if (Input.GetKey(KeyCode.LeftArrow)) {
-                steering -= 0.1f;
-                if (steering < -1.0f) {
-                    steering = -1.0f;
+                m_steering -= 0.1f;
+                if (m_steering < -1.0f) {
+                    m_steering = -1.0f;
                 }
                 steered = true;
             }
@@ -45,15 +65,15 @@ public class CabController : MonoBehaviour
             brake = 1.0f;
         }
         if (steered == false) {
-            if (steering > 0.1f) {
-                steering -= 0.1f;
-            } else if (steering < -0.1f) {
-                steering += 0.1f;
+            if (m_steering > 0.1f) {
+                m_steering -= 0.1f;
+            } else if (m_steering < -0.1f) {
+                m_steering += 0.1f;
             } else {
-                steering = 0.0f;
+                m_steering = 0.0f;
             }
         }
-        Move(steering, accel, brake, brake);
+        Move(m_steering, accel, brake, brake);
     }
 
     public void Move(float steering, float accel, float footbrake, float handbrake) {
@@ -63,20 +83,20 @@ public class CabController : MonoBehaviour
         if (steering < -1.0f) {
             steering = -1.0f;
         }
-        speed += accel;
+        m_speed += accel;
         if (handbrake > 0.0f || footbrake > 0.0f) {
-            speed -= (handbrake + footbrake) * Brake;
+            m_speed -= (handbrake + footbrake) * m_Brake;
         }
-        if (speed > 1.0f) {
-            speed = 1.0f;
+        if (m_speed > 1.0f) {
+            m_speed = 1.0f;
         }
-        if (speed < 0.0f) {
-            speed = 0.0f;
+        if (m_speed < 0.0f) {
+            m_speed = 0.0f;
         }
-        transform.Rotate(new Vector3(0, steering * MaxSteering * speed, 0));
+        transform.Rotate(new Vector3(0, steering * m_MaxSteering * m_speed, 0));
         var forward = transform.forward.normalized;
-        transform.Translate(forward * speed * MaxSpeed * Time.deltaTime, Space.World);
-        animator.SetFloat("direction", steering);
-        animator.SetFloat("speed", speed);
+        transform.Translate(forward * m_speed * m_MaxSpeed * Time.deltaTime, Space.World);
+        m_animator.SetFloat("direction", steering);
+        m_animator.SetFloat("speed", m_speed);
     }
 }
