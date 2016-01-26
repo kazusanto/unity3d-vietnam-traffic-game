500ba6838da6e677b6bfb0e1038ea58ae9027d34
Kazuaki Oshiba <kazusanto@gmail.com>
2016-01-27 00:53:12 +0900

    ignore key-input several second when gameover

--- src/Assets/Scripts/GameOverController.cs
+++ src/Assets/Scripts/GameOverController.cs
@@ -4,37 +4,42 @@ using System.Collections;
 
 public class GameOverController : MonoBehaviour {
 
-    public string SceneName;
+    [SerializeField] private string m_SceneName;
 
-    private Text text;
-    private bool released = false;
+    private Text m_text;
+    private bool m_released = false;
+    private float m_wait_for;
 
     // Use this for initialization
 	void Start () {
-        text = GetComponent<Text>();
-        text.enabled = false;
+        m_text = GetComponent<Text>();
+        m_text.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (PlayerController.Gameover) {
-            text.enabled = true;
-            var restart = false;
-            if (Input.GetMouseButtonDown(0)) {
-                restart = true;
-            }
-            if (Input.GetKey(KeyCode.Space)) {
-                if (released) {
+            if (!m_text.enabled) {
+                m_text.enabled = true;
+                m_wait_for = Time.time + 3.0f;
+            } else if (Time.time > m_wait_for) {
+                var restart = false;
+                if (Input.GetMouseButtonDown(0)) {
                     restart = true;
                 }
-            } else {
-                released = true;
-            }
-            if (restart) {
-                UnityEngine.SceneManagement.SceneManager.LoadScene(SceneName);
+                if (Input.GetKey(KeyCode.Space)) {
+                    if (m_released) {
+                        restart = true;
+                    }
+                } else {
+                    m_released = true;
+                }
+                if (restart) {
+                    UnityEngine.SceneManagement.SceneManager.LoadScene(m_SceneName);
+                }
             }
         } else {
-            text.enabled = false;
+            m_text.enabled = false;
         }
 	}
 }
